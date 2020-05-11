ac8afc425464e80940b0bd7b4260f7c70771a351
Oussema Ben Soltana <oussema.ben.soltana1@gmail.com>
2020-05-11 22:36:01 +0100

    Updates

---
+++ precache-manifest.0a3b0e073d4cbf175d38c0f586a77fc3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658f9ac47785ba5f0c391fc6bff0455f",
+    "url": "/Filmy-app/index.html"
+  },
+  {
+    "revision": "4f26ba02189bcc2acca8",
+    "url": "/Filmy-app/static/css/2.989fd588.chunk.css"
+  },
+  {
+    "revision": "454749c0bc287e824204",
+    "url": "/Filmy-app/static/css/main.789664c7.chunk.css"
+  },
+  {
+    "revision": "4f26ba02189bcc2acca8",
+    "url": "/Filmy-app/static/js/2.198df512.chunk.js"
+  },
+  {
+    "revision": "c560b744118e776c5a9fe14e0546e12a",
+    "url": "/Filmy-app/static/js/2.198df512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "454749c0bc287e824204",
+    "url": "/Filmy-app/static/js/main.2b496037.chunk.js"
+  },
+  {
+    "revision": "0d02d84e8076f78d3c31",
+    "url": "/Filmy-app/static/js/runtime-main.8973e0d9.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Filmy-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Filmy-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Filmy-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Filmy-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Filmy-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
